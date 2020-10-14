249f9ad0d9960f6e6e36bc1ece3c088790be09c4
zahanwasif <zahan.wasif@gmail.com>
2020-10-15 03:55:17 +0500

    Updates

---
+++ precache-manifest.e8d664b2a09bae2bad15e87263bb6fa6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0b4aa4440782785b4d25532f0e65fc6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed8f4034d378ce729c2f",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "36788657c44cfa9a6ff0",
+    "url": "/static/js/2.b3450c66.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.b3450c66.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8f4034d378ce729c2f",
+    "url": "/static/js/main.ebccca86.chunk.js"
+  },
+  {
+    "revision": "e497b455e525a55250f8",
+    "url": "/static/js/runtime-main.e939a389.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
